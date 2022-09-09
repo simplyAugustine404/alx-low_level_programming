2335af0e180c64640790999f95ee596e53db838a
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-09 01:26:10 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >=97; n--)
+	{
+		putchar("n");
+	}
+	putchar("\n");
+
+	return (0);
+}
