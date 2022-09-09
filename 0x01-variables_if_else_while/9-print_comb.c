82314ab604727af7432693a699b001f199116566
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-09 11:20:58 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(44);
+		putchar(32);
+	}
+	return (0)
+}
