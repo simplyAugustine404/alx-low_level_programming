b3acff4b7ec978af69d8c864ad89552888e5ba4d
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-09 01:23:38 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return : A;lways 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar("n");
+	}
+	putchar("\n");
+
+	return (0);
+}
