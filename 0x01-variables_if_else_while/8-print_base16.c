39f6ff6ca818df7baec9e135dc4240e3c15034b8
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-09 01:30:17 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar("n");
+	}
+
+	for (m = 97; m <= 102; m++)
+	{
+		putchar("m");
+	}
+	putchar("\n");
+
+	return (0);
+}
