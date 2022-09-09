f88eedb6f69fc6dfe5d8d0fb2136708a4bd60e32
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-09 01:18:46 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Aleays 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
