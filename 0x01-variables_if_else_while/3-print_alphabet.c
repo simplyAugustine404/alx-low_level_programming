548e11f5d1f9bfbd8570ac8b0eee69c559d65c16
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-09 01:10:10 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -2,16 +2,22 @@
 
 /**
  * main - Entry point
- * Return: Always 0
+ *
+ * Return: Alweays 0
  */
 
 int main(void)
 {
 	int n;
+	int m;
 
 	for (n = 97; n <= 122; n++)
 	{
 		putchar("n");
+	}
+	for (m = 65; m <= 90; m++)
+	{
+		putchar("m");
 	}
 	putchar("\n");
 	return (0);
