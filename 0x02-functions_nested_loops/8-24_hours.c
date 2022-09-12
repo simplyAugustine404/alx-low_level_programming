ff24773518072cbc1d5389de87e569ca694f1c93
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-12 03:21:17 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ */
+
+void jack_bauer(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 23; n++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(58);
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+		}
+	}
+}
