eb085281847250e925cd918b49d24ed005b8e524
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-11 22:27:10 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int l, m, n;
+
+	for (l = 48; l <= 57; l++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			for (n = 48; n<= 57; n++)
+			{
+				if ((m > l) && (n > m))
+				{
+					putchar(l);
+					putchar(m);
+					putchar(n);
+					if (n == 57)
+					{
+						if ((m == 56) && (l == 55))
+						{
+							break;
+						}
+						else
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
