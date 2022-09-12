dd12d6e1cd32a0141329b00119b39abfc314944c
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-12 03:32:28 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @n: first integer
+ * @m: second integer
+ *
+ * Return: Addition result
+ */
+
+int add(int m, int n)
+{
+	ret = (m + n);
+	return (ret);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,6 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add (int n, int m);
+int add (int m, int n);
 void print_to_98(int n);
 void print_times_table(int n);
