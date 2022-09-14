99584f94146f7cac48eb1ede641ce2a2139e0004
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-14 00:40:16 -0700

    98 Battery street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,28 @@
 void print_to_98(int n)
 {
 	if (n == 98)
-		_putchar(n +'0');
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 	else if (n < 98)
 	{
 		for (; n <= 98; n++)
 		{
 			if (n < 9)
 			{
-				_putchar(n + '0');
-				_putchar(44);
-				_putchar(32);
+				if (n < 0)
+				{
+					_putchar(45);
+					_putchar((n * -1) / 10 + '0');
+					_putchar((n * -1) % 10 + '0');
+				}
+				else
+				{
+					_putchar(n + '0');
+					_putchar(44);
+					_putchar(32);
+				}
 			}
 			else
 			{
