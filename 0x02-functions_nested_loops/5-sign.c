26e8ba02a470f56db788159178654444a17cd1d6
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-13 22:55:02 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,8 +12,8 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		_putchar(43);
-		return (1);
+		_putchar(45);
+		return (-1);
 	}
 	else if (n == 0)
 	{
@@ -22,7 +22,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar(45);
-		return (-1);
+		_putchar(43);
+		return (1);
 	}
 }
