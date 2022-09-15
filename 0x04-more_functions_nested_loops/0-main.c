145f73d5c89f9434e3bc47ea97506bdc5524d270
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-15 12:24:12 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return(0);
+}
