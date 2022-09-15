55b4b01b0abe3f2a9cb1d776439f3493ef763993
shawnkiplagat69 <93012956+shawnkiplagat69@users.noreply.github.com>
2022-09-15 12:08:46 +0300

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,11 +6,12 @@
  * @month: month in number format
  * @day: day of the month
  * @year: year
+ * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 ==0))
 	{
 		if (month >= 3 && day >= 60)
 		{
@@ -23,7 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
