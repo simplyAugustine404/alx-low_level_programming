cac59b45bed14627432580b7bed1f5994365fbfe
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-20 10:21:11 -0700

    This report, by its very length, defends itsekf agains the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ * _strlen - return the length of a string
+ *
+ * @s: pointer to string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	char v = *s;
+	int n, i;
+
+	n = 0;
+	while (v[i] != '\0')
+	{
+		n += 1;
+		i++;
+	}
+	return (n);
+}
