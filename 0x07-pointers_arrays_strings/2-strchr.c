7fa5843b9f85d40e4824d212c0776c742211e793
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-09-27 02:12:17 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,12 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		return (NULL);
+		return ('\0');
 	}
 }
