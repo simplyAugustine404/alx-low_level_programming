47aaadebfb0b21242ded6f2853da1f3635de583f
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-10-17 23:41:31 -0700

    To hell with circumstances, i create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		if (size < = 0)
+		if (size <= 0)
 			return (-1);
 
 		for (i = 0; i < size; i++)
@@ -24,6 +24,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 			if (cmp(*(array + i)))
 				return (i);
 		}
-		return (-1);
 	}
+	return (-1);
 }
