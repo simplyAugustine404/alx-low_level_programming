31ae3555e9b6043ae0166033078adcb9c0074f6b
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-10-25 23:55:56 -0700

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - return number of elements
+ * @h: linked list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	for (i = 0; h->next != NULL; i++)
+	{
+		h = h->next;
+	}
+
+	return (i);
+}
