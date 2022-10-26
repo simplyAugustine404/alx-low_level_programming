f30b84d1b2068ec5b99f4bee7c8cff8db48dd203
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-10-25 23:51:48 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(int c);
-size_t print_list(const lits_t *h);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
