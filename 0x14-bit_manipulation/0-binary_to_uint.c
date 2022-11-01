0178ef1495af336e5ebdb92244d792bf342d4085
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-11-01 04:26:39 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui = 0;
+	unsigned int ui;
 	int i, ret;
 
 	if (!b)
 	{
 		return (0);
 	}
+
+	ui = 0;
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
