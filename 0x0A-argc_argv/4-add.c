8e99024daf7ed7adb15900d315bde7d9a8ff8959
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-11-16 05:12:42 -0800

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,12 +14,6 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	if (argc <= 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -29,6 +23,7 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
+		}
 
 		sum += atoi(argv[i]);
 
