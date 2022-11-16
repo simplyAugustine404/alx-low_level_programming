0cf0cdb7c4fc5aabb01c9dedbad86f0d9fcd2e47
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-11-16 05:03:53 -0800

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
