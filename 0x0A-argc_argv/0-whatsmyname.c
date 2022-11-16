923ed6712f6a9e2b5668cc22865179cc6d16a0ff
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-11-16 04:31:18 -0800

    It ain't what they tell you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
