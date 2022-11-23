66844a95510fa23daf272ace8eec86d04adc4893
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-11-22 23:00:33 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	long int previous = 1, next = 2, next, sum = 0;
+
+	while (next < 4000000)
+	{
+		next = current + previous;
+		previous = current;
+		current = next;
+
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
